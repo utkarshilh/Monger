85368388fde99dfde2324688f32ed82158561fed
Yogi <Yogita.shukla@gmail.com>
2023-02-08 14:25:14 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import H from './H';
 
 function App() {
   return (
     <div className="App">
       <h1>This is main page</h1>
+      <H/>
     </div>
   );
 }
